bf9fbf909de5e3b092f3324450d8386f3edabf94
ziyed4 <ziad20-00838@student.eelu.edu.eg>
2024-10-03 20:21:08 +0300

    First Commit

---
+++ src/app/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function($class){
+	    $path= __DIR__ .'/../' . lcfirst(str_replace('\\' , '/' , $class)) . '.php';
+    require $path;
+});
+
+use App\PaymentGateway\Paddle\Transaction;
+
+$paddleTransaction = new Transaction();
+
+var_dump($paddleTransaction);
